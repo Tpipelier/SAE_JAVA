3b8bb09bd4f7e28b10af51b932eb3ba883587292
CHAZAL ALEXIS p2500558 <p2500558@univ-lyon1.fr>
2026-05-20 18:00:17 +0200

    Création de la classe pour créé des graphes

---
+++ Manipulation de Graphes/src/Graphes/MonGraphe.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Graphes;
+
+import org.graphstream.graph.implementations.SingleGraph;
+
+/**
+ *
+ * @author p2500558
+ */
+public class MonGraphe extends SingleGraph {
+    
+    public MonGraphe(String nom) {
+        super(nom);
+    }
+    
+    public static void chargerGraphe(String nom_fichier){
+        
+    }
+    
+    public static void construireGrapheAleatoire(int nbSommets){
+        
+    }
+
+    
+}
