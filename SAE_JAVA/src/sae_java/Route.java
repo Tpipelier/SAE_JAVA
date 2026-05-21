4302a8db05bc2cf78b477dd29e8aa145f4e936b2
unknown <theo.pipelier@gmail.com>
2026-05-21 11:12:29 +0200

    essaie plus concluant mais non fonctionnel de lecture de graphe puis remplissement d'une structure plus adapté en java avec les informations obtenus

--- SAE_JAVA/src/sae_java/Route.java
+++ SAE_JAVA/src/sae_java/Route.java
@@ -9,16 +9,38 @@ package sae_java;
  * @author compteadmin
  */
 public class Route {
-    public String name;
-    public double valuation;
+    String name;
+    double fiabilité;
+    int distance;
+    int durée;
     private Sommet sDépart;
     private Sommet sArrivée;
 
-    public Route(String name, double valuation, Sommet sDépart, Sommet sArrivée) {
+    public Route(String name, double fiabilité, int distance, int durée, Sommet sDépart, Sommet sArrivée) {
         this.name = name;
-        this.valuation = valuation;
+        this.fiabilité = fiabilité;
+        this.distance = distance;
+        this.durée = durée;
         this.sDépart = sDépart;
         this.sArrivée = sArrivée;
+    }
+
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public double getValuation() {
+        return valuation;
+    }
+
+    public Sommet getsDépart() {
+        return sDépart;
+    }
+
+    public Sommet getsArrivée() {
+        return sArrivée;
     }
     
     
--- SAE_JAVA/src/sae_java/SAE_JAVA.java
+++ SAE_JAVA/src/sae_java/SAE_JAVA.java
@@ -19,25 +19,84 @@ public class SAE_JAVA {
      */
     public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
-        
+
         FileInputStream file = new FileInputStream("C:/Users/compteadmin/Desktop/SAE_JAVA/data/graphe1.csv");
         Scanner scanner = new Scanner(file);
-        Sommet[] tabS =new Sommet[compteSommet("C:/Users/compteadmin/Desktop/SAE_JAVA/data/graphe1.csv")];
-        int i=0;
-        //renvoie true tant qu'il y a une autre ligne à lire
+        Sommet[] tabS = new Sommet[compteSommet("C:/Users/compteadmin/Desktop/SAE_JAVA/data/graphe1.csv")];
+        Route[] tabR = new Route[compteRoute("C:/Users/compteadmin/Desktop/SAE_JAVA/data/graphe1.csv")];
+        int i = 0;
         while (scanner.hasNext()) {
+
             String ligne = scanner.nextLine();
             String[] tokens = ligne.split(";");
-            if(ligne.compareTo("// Graphe planaire maximal;")!=0 && ligne.compareTo("// 10 sommets")!=0 ){
-                tabS[i]= new Sommet(tokens[0],tokens[1]);
-                System.out.println(tabS[i].getType()+" "+tabS[i].getNom());
+            if (ligne.compareTo("// Graphe planaire maximal;") != 0 && ligne.compareTo("// 10 sommets") != 0) {
+                tabS[i] = new Sommet(tokens[0], tokens[1]);
+//                System.out.println(tabS[i].getType()+" "+tabS[i].getNom());
+                i++;
             }
-            
-            
+
 //            tab[i] = new Commune(Integer.parseInt(tokens[0]), tokens[1], tokens[2]);
-            i++;
         }
+        scanner.close(); // /!\ Ceci ferme 'scanner' ET 'file' !
+
+// --- DEUXIÈME LECTURE ---
+// Tu DOIS recréer un nouveau FileInputStream ici
+        file = new FileInputStream("C:/Users/compteadmin/Desktop/SAE_JAVA/data/graphe1.csv");
+        scanner = new Scanner(file); // Là, ça va marcher !
+        System.out.println("open");
+        i = 0;
+        int k = 0;
+        int indexArrivee;
+        int j;
+        while (scanner.hasNext()) {
+
+            j = 2;
+            String ligne = scanner.nextLine();
+            String[] tokens = ligne.split(";");
+            
+            
+            if (ligne.compareTo("// Graphe planaire maximal;") != 0 && ligne.compareTo("// 10 sommets") != 0) {
+                //System.out.println(tabS[i].getType()+" "+tabS[i].getNom());
+                while (j < tokens.length) {
+                    indexArrivee=j-2;
+                    String[] triplet = tokens[j].split(",");
+                    tabR[k] = new Route("R" + j, Double.parseDouble(tokens[0]),Integer.parseInt(tokens[1]),Integer.parseInt(tokens[2]), tabS[i], tabS[indexArrivee]);
+                    System.out.println(tabR[k].getName() + " " + tabR[k].getValuation() + " " + tabR[k].getsArrivée() + " " + tabR[k].getsDépart());
+                    j++;
+                    k++;
+                }
+                i++;
+
+            }
+
+//            tab[i] = new Commune(Integer.parseInt(tokens[0]), tokens[1], tokens[2]);
+        }
+
+        //System.out.println(compteRoute("C:/Users/compteadmin/Desktop/SAE_JAVA/data/graphe1.csv"));
         scanner.close();
+    }
+
+    public static int compteRoute(String nomFichier) {
+        int cpt = 0;
+        try {
+            // Le fichier d'entrée
+            FileInputStream file = new FileInputStream(nomFichier);
+            Scanner scanner = new Scanner(file);
+
+            //renvoie true tant qu'il y a une autre ligne à lire
+            while (scanner.hasNext()) {
+                String ligne = scanner.nextLine();
+                String[] tokens = ligne.split(";");
+                cpt = cpt + tokens.length - 2;
+
+            }
+
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+        return cpt;
     }
 
     public static int compteSommet(String nomFichier) {
@@ -59,11 +118,10 @@ public class SAE_JAVA {
         }
         return cpt;
     }
-   
 
     public static Route[] chargerGraphes(String nom_fichier) throws FileNotFoundException {
         Route[] tab = new Route[compteSommet(nom_fichier)];
-        Sommet[] tabS =new Sommet[compteSommet(nom_fichier)];
+        Sommet[] tabS = new Sommet[compteSommet(nom_fichier)];
         int i = 0;
 
         // Le fichier d'entrée
@@ -75,7 +133,7 @@ public class SAE_JAVA {
             String ligne = scanner.nextLine();
             System.out.println(ligne);
             String[] tokens = ligne.split(";");
-            tabS[i]= new Sommet(tokens[0],tokens[1]);
+            tabS[i] = new Sommet(tokens[0], tokens[1]);
 //            tab[i] = new Route(Integer.parseInt(tokens[0]), tokens[1], tokens[2]);
             i++;
         }
