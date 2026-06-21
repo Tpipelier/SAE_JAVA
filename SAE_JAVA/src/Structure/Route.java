76971098638c1f6aacaf06dc34a41d38cb529271
Alexis <alexis.chazal38@gmail.com>
2026-06-21 11:36:15 +0200

    beaucoup d'essais pour corriger les filtres sans reussir

--- SAE_JAVA/src/Structure/Route.java
+++ SAE_JAVA/src/Structure/Route.java
@@ -49,7 +49,35 @@ public class Route {
     public Sommet getSommetArrivee() {
         return sArrivee;
     }
+    
+    public void setSommetDepart(Sommet sDepart) {
+        this.sDepart = sDepart;
+    }
 
+    public void setSommetArrivee(Sommet sArrivee) {
+        this.sArrivee = sArrivee;
+    }
+
+    public Route trouverRouteParNoms(Graphe global, String nom1, String nom2) {
+    int nbSommetsGlobaux = global.getNbSommet();
+    
+    for (int i = 0; i < nbSommetsGlobaux; i++) {
+        for (int j = 0; j < nbSommetsGlobaux; j++) {
+            Route r = global.getRoute(i, j);
+            
+            if (r != null) {
+                String dep = r.getSommetDepart().getNom();
+                String arr = r.getSommetArrivee().getNom();
+                
+                // On vérifie si la route relie bien nom1 et nom2 (dans un sens ou dans l'autre)
+                if ((dep.equals(nom1) && arr.equals(nom2)) || (dep.equals(nom2) && arr.equals(nom1))) {
+                    return r;
+                }
+            }
+        }
+    }
+    return null; // Aucune route n'existe entre ces deux villes
+}
     
 
 }
